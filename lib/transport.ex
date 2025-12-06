ebed720d0ed12c98d9da55931650543b123ddb1b
Samuel Ventura <samuel.ventura@yeico.com>
2025-12-06 02:15:15 -0600

    Modbus functional transport for applet custom transport.

--- lib/transport.ex
+++ lib/transport.ex
@@ -1,30 +1,50 @@
 defmodule Modbus.Transport do
   @moduledoc false
   @callback open(opts :: keyword()) ::
-              {:ok, id :: any()} | {:error, reason :: any()}
-  @callback readp(id :: any()) :: {:ok, packet :: binary()} | {:error, reason :: any()}
-  @callback readn(id :: any(), count :: integer(), timeout :: integer()) ::
+              {:ok, conn :: any()} | {:error, reason :: any()}
+  @callback readp(conn :: any()) :: {:ok, packet :: binary()} | {:error, reason :: any()}
+  @callback readn(conn :: any(), count :: integer(), timeout :: integer()) ::
               {:ok, packet :: binary()} | {:error, reason :: any()}
-  @callback write(id :: any(), packet :: binary()) :: :ok | {:error, reason :: any()}
-  @callback close(id :: any()) :: :ok | {:error, reason :: any()}
+  @callback write(conn :: any(), packet :: binary()) :: :ok | {:error, reason :: any()}
+  @callback close(conn :: any()) :: :ok | {:error, reason :: any()}
 
-  def open(mod, opts) do
+  def open(mod, opts) when is_atom(mod) do
     mod.open(opts)
   end
 
-  def readn({mod, id}, count, timeout) do
-    mod.readn(id, count, timeout)
+  def open(mod, opts) when is_function(mod, 1) do
+    mod.({:open, opts})
   end
 
-  def readp({mod, id}) do
-    mod.readp(id)
+  def readn({mod, conn}, count, timeout) when is_atom(mod) do
+    mod.readn(conn, count, timeout)
   end
 
-  def write({mod, id}, packet) do
-    mod.write(id, packet)
+  def readn({mod, conn}, count, timeout) when is_function(mod, 1) do
+    mod.({:readn, conn, count, timeout})
   end
 
-  def close({mod, id}) do
-    mod.close(id)
+  def readp({mod, conn}) when is_atom(mod) do
+    mod.readp(conn)
+  end
+
+  def readp({mod, conn}) when is_function(mod, 1) do
+    mod.({:readp, conn})
+  end
+
+  def write({mod, conn}, packet) when is_atom(mod) do
+    mod.write(conn, packet)
+  end
+
+  def write({mod, conn}, packet) when is_function(mod, 1) do
+    mod.({:write, conn, packet})
+  end
+
+  def close({mod, conn}) when is_atom(mod) do
+    mod.close(conn)
+  end
+
+  def close({mod, conn}) when is_function(mod, 1) do
+    mod.({:close, conn})
   end
 end
